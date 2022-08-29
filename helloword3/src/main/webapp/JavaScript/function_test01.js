1d42778c24d48a8f795059e7db0e06004cfdb810
admin <admin@YD-22>
2022-08-29 17:51:03 +0900

    20220829

--- helloword3/src/main/webapp/JavaScript/function_test01.js
+++ helloword3/src/main/webapp/JavaScript/function_test01.js
@@ -1,18 +1,27 @@
 
-function sum(num1,num2){
-    return num1+num2
+
+let myFunc= function (pp){
+    console.log('안녕'+ pp)
 }
 
-function sum2 (num3,num4){
-    return num3+num4
+
+myFunc('고구마야');
+
+
+(function tt(){
+    console.log('우하하하')
+})();
+
+// tt();
+
+mySum(1,2,3,4,5,7)
+
+function mySum5(a,b){
+    console.log(a+b)
 }
 
-function mina (num3,num4){
-    return num3-num4
-}
+// setTimeout(function(a,b){
+//     mySum5(a+b)
+// },2000)
 
-document.write(sum(5,sum2('고기',sum(1,8))))
 
-document.write(sum())
-
-document.write('<br>'+mina('고기고기','고기'))
